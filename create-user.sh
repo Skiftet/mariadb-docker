73adfbdba9c1514e788dc6e036972c614df1b2c3
Joel Ekström Svensson <JoelESvensson@users.noreply.github.com>
2017-05-14 00:16:09 +0200

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -4,9 +4,9 @@ set -e
 RANDOM_PASSWORD=$(cat /dev/urandom | tr -dc A-Za-z0-9 | fold -w 16 | head -n 1)
 
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE USER '$1'@'localhost' IDENTIFIED BY '$RANDOM_PASSWORD';
+    CREATE USER '$1'@'%' IDENTIFIED BY '$RANDOM_PASSWORD';
     CREATE DATABASE $1;
-    GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';
+    GRANT ALL PRIVILEGES ON $1.* TO '$1'@'%';
     FLUSH PRIVILEGES;
 EOSQL
 
